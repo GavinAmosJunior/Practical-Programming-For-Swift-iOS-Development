cfb8de90bc9add42bca45388029c2817e1a5f27e
GavinAmosJunior <gavinamosj@gmail.com>
2025-10-01 17:50:57 +0700

    i learned about how "variables" are mutable, and that "let" is a constant

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 var greeting = "Hello, playground" //variables are mutable, its value can change down the line
-print (greeting)
 greeting = "Hello World"
+greeting = "What's Up?"
+print (greeting)
 
 let numbers = 1 //let is a constant, its value cannot change
 print(numbers)
