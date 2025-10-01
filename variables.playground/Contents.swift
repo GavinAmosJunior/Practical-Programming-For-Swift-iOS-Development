03f35b97afbdfb8f5f49df4c5460073e8b81685c
GavinAmosJunior <gavinamosj@gmail.com>
2025-10-01 17:57:10 +0700

    declare the data type explicitly

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -3,7 +3,10 @@ import UIKit
 var greeting = "Hello, playground" //variables are mutable, its value can change down the line
 greeting = "Hello World"
 greeting = "What's Up?"
-print (greeting)
+print (greeting) //prints "What's Up"
 
 let numbers = 1 //let is a constant, its value cannot change
-print(numbers)
+print(numbers) //prints "1"
+
+var name: String = "Gavin" //we can declare the type of data it is to be even more specific
+
