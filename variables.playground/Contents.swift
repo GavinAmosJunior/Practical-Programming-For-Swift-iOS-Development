5d24673885aaa9dbb8a6fb1eb6b781674ebcfb09
GavinAmosJunior <gavinamosj@gmail.com>
2025-10-01 17:47:38 +0700

    Initial Commit

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground" //variables are mutable, its value can change down the line
+print (greeting)
+greeting = "Hello World"
+
+let numbers = 1 //let is a constant, its value cannot change
+print(numbers)
