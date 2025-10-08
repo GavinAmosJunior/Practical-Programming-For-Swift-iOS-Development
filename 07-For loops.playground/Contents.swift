ae24080004ddaefb48259ec3f2b7840b43054ef3
Gavin Amos Junior <gavinamosj@gmail.com>
2025-10-08 19:47:50 +0700

    For loop / For in loops

---
+++ 07-For loops.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+//FOR LOOP
+//for loops are used to repeat code a specific number of times
+
+
+                        // Loop through numbers 1 to 5
+for i in 1...5 {        // 'i' takes each value in the range one by one: 1, 2, 3, 4, then 5
+    print(i)            // Prints the current value of i
+    
+    
+}
+
+//FOR IN LOOP
+//for-in loops are used to loop through items in a collection (arrays, dictionary)
+
+// Create an array of fruits
+let fruits = ["apple", "banana", "cherry"]
+
+
+                            // Loop through each item in the 'fruits' array
+for fruit in fruits {       // 'fruit' takes on each element in the array one by one:
+    print(fruit)            // Prints the current fruit
+}
