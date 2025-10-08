b4d13055f95ebbf08e3d66d9755416fe5e71ee52
Gavin Amos Junior <gavinamosj@gmail.com>
2025-10-08 20:02:19 +0700

    While loops

---
+++ 08-While loops.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+//WHILE LOOPS
+//loops based on condition
+
+//create a place holder variable
+var i = 0
+
+
+//condition = while as long as i is less then 1 (run the code below)
+while i < 10 {
+    print(i)            //prints out the current value of i
+    i += 1              //increments i (1+) each loop
+}
